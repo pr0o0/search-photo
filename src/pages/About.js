c5300f2200ec808a861cf0a78fa67636db0feca3
linyincheng <uzumakigodzilla@gmail.com>
2023-01-20 20:55:18 +0800

    test edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div style={{ minHeight: "90vh" }}>About</div>;
+  return <div style={{ minHeight: "90vh" }}>About </div>;
 };
 
 export default About;
